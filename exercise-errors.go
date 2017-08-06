172971a00248578359ae2f6377c04106cd0ca014
aamir <aamirngr610@gmail.com>
2017-08-06 08:59:31 -0700

    Update exercise-errors.go

--- exercise-errors.go
+++ exercise-errors.go
@@ -4,30 +4,16 @@ import (
 	"fmt"
 	"math"
 )
+type lo string
+func (x lo) Error() string{
+  return fmt.Sprintf(string(x))
 
-type ErrNegativeSqrt float64
-
-func (e ErrNegativeSqrt) Error() string {
-	return fmt.Sprintf("can not Sqrt negative number:%v", float64(e))
 }
-
-/*
 func Sqrt(x float64) (float64, error) {
-    return 0, nil
-}
-*/
-func Sqrt(x float64) (float64, error) {
-	if x < 0 {
-		return 0, ErrNegativeSqrt(x)
-	}
-	z := 1.0
-	for {
-		if math.Abs(z-(z-(z*z-x)/(z*2))) < 0.00000000000001 {
-			return z, nil
-		} else {
-			z = z - (z*z-x)/(z*2)
-		}
-	}
+	if x>=0{return math.Sqrt(x),nil
+		   
+		   }else{
+				return 0, lo("not allowed")}
 }
 
 func main() {
